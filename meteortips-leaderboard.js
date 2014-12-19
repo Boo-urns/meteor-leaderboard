8130af9abc0a58b9db4f6a0dffb5e4be4942e501
Corey Burns <coreyb@simantel.com>
2014-12-19 10:57:51 -0600

    End of Ch:10 Accounts

--- meteortips-leaderboard.js
+++ meteortips-leaderboard.js
@@ -1,12 +1,17 @@
-PlayersList = new Mongo.Collection('players');
+//UserAccounts = new Mongo.Collection('users');
+PlayersList  = new Mongo.Collection('players');
+
 if (Meteor.isClient) {
   Template.leaderboard.helpers({
     player: function(){
-      return PlayersList.find({}, { sort: {score: -1, name: 1} });
+      var currentUserId = Meteor.userId();
+      return PlayersList.find({createdBy: currentUserId}, 
+                              {sort: {score: -1, name: 1} });
 
     },
     numPlayers: function(){
-      return PlayersList.find().count();
+      var currentUserId = Meteor.userId();
+      return PlayersList.find({createdBy: currentUserId}).count();
     },
     selectedClass: function(){
       var playerId = this._id;
@@ -27,6 +32,10 @@ if (Meteor.isClient) {
       //var selectedPlayer = Session.get('selectedPlayer');
       //PlayersList.update({_id: playerId}, {$set: {score: 100}});
     },
+    'click .remove': function(){
+      var selectedPlayer = Session.get('selectedPlayer');
+      PlayersList.remove(selectedPlayer);
+    },
     'click .increment': function() {
       var selectedPlayer = Session.get('selectedPlayer');
       PlayersList.update(selectedPlayer, {$inc: {score: 5}});
@@ -35,6 +44,21 @@ if (Meteor.isClient) {
       var selectedPlayer = Session.get('selectedPlayer');
       PlayersList.update(selectedPlayer, {$inc: {score: -5}});
     }
+  });
+
+
+  Template.addPlayerForm.events({
+    // events go here
+    'submit form': function(e){
+      e.preventDefault();
+      var playerNameVar = e.target.playerName.value;
+      var currentUserId = Meteor.userId();
+      PlayersList.insert({
+        name: playerNameVar,
+        score: 0,
+        createdBy: currentUserId
+      });
+    },
   });
 }
 
